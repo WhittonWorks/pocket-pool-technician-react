fb51b059d4f5617de948880a1ed08a11e23f9a88
WhittonWorks <whittonworksllc@gmail.com>
2025-09-30 08:43:11 -0500

    Update gitignore and add capacitor.config.ts

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'net.whittonworks.ppt',
+  appName: 'Pocket Pool Technician',
+  webDir: 'build'
+};
+
+export default config;
